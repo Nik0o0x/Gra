49ad9c0941a9adef6dcd5b592e936a25838c5d8a
3108917 <3108917@TE-GDA-D-D12C25>
2022-03-11 12:41:49 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,61 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        bool playAgain = true;
+        int min = 1;
+        int max = 10;
+        int guess;
+        int number;
+        int guesses;
+        String response;
+
+        while (playAgain)
+        {
+            guess = 0;
+            guesses = 0;
+            response = "";
+            number = random.Next(min, max + 1);
+
+            while (guess != number)
+            {
+                Console.WriteLine("zgadnij liczbe pomiędzy " + min + " - " + max + " : ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(" zgadnij: " + guess);
+
+                if (guess > number)
+                {
+                    Console.WriteLine(guess + " liczba jest za duża!");
+
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine(guess + " liczba jest za mała!");
+                }
+                guesses++;
+            }
+            Console.WriteLine("liczba: " + number);
+            Console.WriteLine("Wygrałeś!!!");
+            Console.WriteLine("trafienia: " + guesses);
+
+            Console.WriteLine("chcesz grać jeszcze raz (T/N): ");
+            response = Console.ReadLine();
+            response = response.ToUpper();
+
+            if (response == "T")
+            {
+                playAgain = true;
+            }
+            else
+            {
+                playAgain = false;
+            }
+
+            Console.WriteLine("dzięki za gre");
+            Console.ReadKey();
+        }
+
+    }
+
+}
